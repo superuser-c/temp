7f5ba050f2b4213663e06431cab1dc94f344b48c
superuser-c <62846333+superuser-c@users.noreply.github.com>
2020-06-28 18:42:36 +0200

    Create Shader.h

---
+++ GLWrap/Shader.h
@@ -0,0 +1,101 @@
+#pragma once
+#ifndef JT_SHADER_H
+#define JT_SHADER_H
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "../loader/loader.h"
+
+class Shader {
+public:
+	unsigned int ID;
+
+	inline Shader(std::string vert_path, std::string frag_path, std::string geom_path = "") {
+		std::string verts;
+		std::string frags;
+		std::string geoms;
+		try {
+			verts = get_file_contents(vert_path.c_str());
+			frags = get_file_contents(frag_path.c_str());
+			if (!geom_path.empty())
+				geoms = get_file_contents(geom_path.c_str());
+		} catch (std::exception) {
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
+		const char *vShaderCode = verts.c_str();
+		const char *fShaderCode = frags.c_str();
+		unsigned int vertex, fragment, geometry;
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+		checkCompileErrors(vertex, "VERTEX");
+		fragment = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment, 1, &fShaderCode, NULL);
+		glCompileShader(fragment);
+		checkCompileErrors(fragment, "FRAGMENT");
+		if (!geom_path.empty()) {
+			const char *gShaderCode = geoms.c_str();
+			geometry = glCreateShader(GL_GEOMETRY_SHADER);
+			glShaderSource(geometry, 1, &gShaderCode, NULL);
+			glCompileShader(geometry);
+			checkCompileErrors(geometry, "GEOMETRY");
+		}
+		ID = glCreateProgram();
+		glAttachShader(ID, vertex);
+		glAttachShader(ID, fragment);
+		if (!geom_path.empty())
+			glAttachShader(ID, geometry);
+		glLinkProgram(ID);
+		checkCompileErrors(ID, "PROGRAM");
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+		if (!geom_path.empty())
+			glDeleteShader(geometry);
+	}
+	inline ~Shader() {
+		glDeleteProgram(ID);
+	}
+	inline void use() {
+		glUseProgram(ID);
+	}
+	inline void setBool(const std::string &name, bool value) const {
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+	}
+	inline void setInt(const std::string &name, int value) const {
+		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	}
+	inline void setFloat(const std::string &name, float value) const {
+		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+	inline void setMat4(const std::string &name, glm::mat4 value) {
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()),
+			1, GL_FALSE, glm::value_ptr(value));
+	}
+private:
+	inline void checkCompileErrors(unsigned int shader, std::string type) {
+		int success;
+		char infoLog[1024];
+		if (type != "PROGRAM") {
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success) {
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << 
+					type << "\n" << infoLog << 
+					"\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		} else {
+			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+			if (!success) {
+				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << 
+					type << "\n" << infoLog <<
+					"\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		}
+	}
+};
+#endif
